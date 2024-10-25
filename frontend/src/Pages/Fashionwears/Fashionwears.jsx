57cbf0478dd73120b9c4be7d1d93764e5cbc0519
soumil7 <121105628+soumil7@users.noreply.github.com>
2024-10-25 18:31:32 +0530

    Update Fashionwears.jsx

--- frontend/src/Pages/Fashionwears/Fashionwears.jsx
+++ frontend/src/Pages/Fashionwears/Fashionwears.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './FashionWears.css';
+import './Fashionwears.css';
 import { FaBookmark, FaRegBookmark } from 'react-icons/fa';
 import FilterButton from '../../Components/FilterButton/FilterButton';
 
